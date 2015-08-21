0b84a9137941152d0dc3cc9107ce3dbc6f90357e
Sasha Egorov <a.a.egoroff@gmail.com>
2015-08-21 22:49:11 +0400

    Aliases cleaned up

--- aliases.sh
+++ aliases.sh
@@ -1,14 +1,9 @@
 # Алиасы для RVM.
-alias rvg='rvm use 1.9.3@global'
-alias rvr='rvm use 1.9.3@rails4'
+alias rvg='rvm use ruby-2.1.6@global'
+alias rvr='rvm use ruby-2.1.6@rails'
 alias rvc='rvm current'
-alias rvl='rvm list'
-alias rvlk='rvm list known'
-alias rvgl='rvm gemset list'
 
 # Алиасы для RubGems
-alias gec='gem cleanup'
-alias gei='gem install'
 alias gel='gem list'
 alias geu='gem update'
 alias geus='gem update --system'
@@ -16,10 +11,8 @@ alias ges='gem search --remote'
 
 # Алиасы для Git
 alias gits='git status --short'
-alias gitss='git status'
 alias gitd='git diff'
 alias gitdc='git diff --cached'
-alias gitc='git commit'
 alias gita='git add'
 alias gitai='git add --interactive'
 alias giti='git instaweb'
@@ -27,22 +20,4 @@ alias gitl='git log'
 alias gitlo='git log --oneline'
 alias gitla='git log --pretty --stat HEAD^..HEAD'
 alias gitck='git checkout'
-alias gitp='git pull'
-
-# Не знаю, как сделать так, чтобы Git показывал неотслеживаемые файлы,
-# а не только каталоги. Вывод неотслеживаемых файлов
-gitu () {
-  local UNTRACKED
-  for UNTRACKED in `git status -s | awk '{print $2}'`; do
-    if [ -d ${UNTRACKED} ]; then
-      git status -s ${UNTRACKED};
-    fi;
-  done
-}
-# Удаление неотслеживаемых файлов
-gitud () {
-  local UNTRACKED_FILE
-  for UNTRACKED_FILE in `gitu`; do
-    rm -i ${UNTRACKED_FILE};
-  done
-}
+alias gitc='gitlo --color| head -5;git commit'
