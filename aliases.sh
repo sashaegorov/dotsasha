3f1a393fc06387a7851cf1c1ba6c4cdce4c7b4e9
Alexander Egorov <sashaegorov@fubar.netcracker.com>
2015-09-28 16:09:29 +0400

    aliases is changing

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 # Atom alias : - )
-alias a='atom'
+alias a='atom .'
 
 # RVM aliases
 alias rg='rvm use ruby-2.2.3@global'
@@ -16,6 +16,7 @@ alias geus='gem update --system'
 alias gs='git status --short'
 alias gd='git diff'
 alias gdc='git diff --cached'
+alias gds='git diff --stat'
 alias ga='git add'
 alias gai='git add --interactive'
 alias gi='git instaweb'
@@ -30,11 +31,15 @@ alias be='bundle exec'
 
 # Rails 4 aliases
 # http://matthewhutchinson.net/2010/9/19/rails-3-bash-aliases-and-irbrc-configs
+alias t='export RAILS_ENV=test; echo -e Rails environment is: ${COLY}${RAILS_ENV}${COLD}'
+alias d='export RAILS_ENV=development; echo -e Rails environment is: ${COLG}${RAILS_ENV}${COLD}'
+alias p='export RAILS_ENV=production; echo -e Rails environment is: ${BLDR}${RAILS_ENV}${COLD}'
+
 alias rake='spring rake'
 alias r='be spring rails'
 alias rat='be spring rake -T'
 alias rar='be spring rake routes'
-alias radm='be spring rake db:migrate'
+alias db='be spring rake db:drop db:create db:migrate'
 
 # Rails logs, tailing and cleaning
 alias cdl='> ./log/development.log'
