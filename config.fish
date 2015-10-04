ce12aafb273ab1efd03fc1cb564085b6aa798901
Sasha Egorov <a.a.egoroff@gmail.com>
2015-10-04 07:33:31 +0400

    config.fish versioned

---
+++ config.fish
@@ -0,0 +1,7 @@
+test -s ~/.nvm-fish/nvm.fish; and source ~/.nvm-fish/nvm.fish
+# Path to your oh-my-fish.
+set -g OMF_PATH $HOME/.local/share/omf
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+# Load fishmarks (http://github.com/techwizrd/fishmarks)
+. $HOME/.fishmarks/marks.fish
