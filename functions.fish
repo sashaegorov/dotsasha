bf646b66851152c4f8e9ec45494f85e2546aea65
Sasha Egorov <a.a.egoroff@gmail.com>
2015-12-28 02:26:21 +0400

    Password generator updated

--- functions.fish
+++ functions.fish
@@ -1,8 +1,8 @@
 # Generate nice and secure password
 # Example: genpass 32
 function -d 'Generate nice and secure password' genpass;
-  [ -z $argv ]; or set -l l $argv[1]
-  for i in (seq 6)
+  [ -z $argv ]; and set -l l 16
+  for i in (seq 5)
     gtr -cd 'A-Za-z0-9_!@#$%^&*()_' < /dev/urandom | \
     head -c {$l} | xargs echo
   end
