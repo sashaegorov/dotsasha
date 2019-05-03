279cdb46094e9f3f8a16ff800f9d6c07090bce9f
Sasha Egorov <a.a.egoroff@gmail.com>
2019-05-03 16:39:30 +0400

    Fish improvements

--- config.fish
+++ config.fish
@@ -1,13 +1,21 @@
+set -x LANG en_US.UTF-8
+
 # Add custom bin path
 set -U fish_user_paths $HOME/.bin $fish_user_paths
 
+set -g theme_color_scheme solarized
 set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
 set -g theme_title_display_process yes
 set -g theme_date_format "+%H:%M"
 set -g theme_display_vagrant yes
 set -g theme_display_ruby yes
-set -g theme_color_scheme terminal-dark
 set -g theme_project_dir_length 1
+set -g theme_display_k8s_context no
+set -g theme_project_dir_length 1
+set -g theme_newline_cursor yes
+set -g default_user alexanderegorov
+set -g theme_show_exit_status yes
 
 # nvm Integration
 test -s ~/.nvm-fish/nvm.fish;
--- functions.fish
+++ functions.fish
@@ -11,3 +11,33 @@ function genpass -d 'Generate nice and secure password';
     head -c {$l} | xargs echo
   end
 end
+
+function rails
+  if test -f bin/rails
+    bin/rails $argv
+  else if test -f Gemfile
+    bundle exec rails $argv
+  else
+    command rails $argv
+  end
+end
+
+function rake
+  if test -f bin/rake
+    bin/rake $argv
+  else if test -f Gemfile
+    bundle exec rake $argv
+  else
+    command rake $argv
+  end
+end
+
+function rspec
+  if test -f bin/rspec
+    bin/rspec $argv
+  else if test -f Gemfile
+    bundle exec rspec $argv
+  else
+    command rspec $argv
+  end
+end
